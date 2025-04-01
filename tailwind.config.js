5807c7577a2c18d0520020ff372268d810667ec9
sbp240103 <154756072+sbp240103@users.noreply.github.com>
2025-04-01 17:43:49 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files in the src directory
+        "./public/index.html"         // Include the main HTML file
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
